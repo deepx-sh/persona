c99ead561ca2be3fbf96f264bd7404aaaced7cf6
Deep Prajapati <dvprajapati7212@gmail.com>
2026-09-05 20:43:36 +0530

    feat: implement ApiResponse class for standardized API responses

---
+++ server/src/utils/ApiResponse.ts
@@ -0,0 +1,13 @@
+export class ApiResponse<T = unknown>{
+    public success: boolean;
+    public statusCode: number;
+    public message: string;
+    public data: T;
+
+    constructor(statusCode: number, data: T, message = "Success") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+}
