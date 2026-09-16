96b414849a69e55de4720061f14d6f6b1b9fc3eb
Deep Prajapati <dvprajapati7212@gmail.com>
2026-09-16 21:05:53 +0530

    feat: add analyzeContactProfile route for contact analysis

--- server/src/routes/contact.route.ts
+++ server/src/routes/contact.route.ts
@@ -3,7 +3,7 @@ import { requireAuth } from "../middlewares/requireAuth.middleware.js";
 import { csvUpload } from "../config/multer.js";
 
 import { uploadContactsCSV, getContacts, getContactById, updateContact, deleteContact } from "../controllers/contact.controller.js";
-
+import { analyzeContactProfile } from "../controllers/profileAnalyzer.controller.js";
 const router = Router()
 
 router.use(requireAuth)
@@ -11,6 +11,7 @@ router.use(requireAuth)
 router.post("/upload", csvUpload.single("file"), uploadContactsCSV)
 router.get("/", getContacts)
 router.get("/:id", getContactById);
+router.post("/:id/analyze",analyzeContactProfile)
 router.patch("/:id", updateContact)
 router.delete("/:id",deleteContact)
 
